d28481c8aab3821e14da762c1792a3acaed168de
Chris Blanquera <chris@incept.asia>
2026-03-07 01:57:00 +0800

    better mysql example

--- examples/with-mysql2/src/index.ts
+++ examples/with-mysql2/src/index.ts
@@ -12,15 +12,19 @@ async function main() {
   const engine = connect(resource);
 
   const create = engine.create('profile')
-    .addField('id', { type: 'VARCHAR', length: 255 })
+    .addField('id', { type: 'int', autoIncrement: true })
     .addField('name', { type: 'VARCHAR', length: 255 })
-    .addPrimaryKey('id');
+    .addField('age', { type: 'int', length: 2, nullable: true })
+    .addField('price', { type: 'float', length: [ 10, 2 ], unsigned: true, nullable: true })
+    .addField('created', { type: 'datetime', default: 'now()' })
+    .addPrimaryKey('id')
+    .addUniqueKey('name', 'name');
   console.log(create.query());
   console.log(await create);
 
   const insert = engine
     .insert('profile')
-    .values({ id: '1', name: 'John Doe' });
+    .values({ name: 'John Doe' });
   console.log(insert.query());
   console.log(JSON.stringify(await insert, null, 2));
 
@@ -31,14 +35,14 @@ async function main() {
   const update = engine
     .update('profile')
     .set({ name: 'Jane Doe' })
-    .where('id = ?', [ '1' ]);
+    .where('id = ?', [ 1 ]);
   console.log(update.query());
   console.log(JSON.stringify(await update, null, 2));
   console.log(JSON.stringify(await select, null, 2));
 
   const remove = engine
     .delete('profile')
-    .where('id = ?', [ '1' ]);
+    .where('id = ?', [ 1 ]);
   console.log(remove.query());
   console.log(JSON.stringify(await remove, null, 2));
   console.log(JSON.stringify(await select, null, 2));
